2feccc66d4769f55055253405757c4f38bdb7c3e
Devalyo <danieldevalho@gmail.com>
2025-03-06 15:10:05 -0300

    feat: setup custom tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 function App() {
   return (
-    <div className="flex align-middle items-center justify-center bg-amber-500">App</div>
+    <>
+      <div className="flex align-middle items-center justify-center bg-red-800">App</div>
+      <h1 className="bg-semanticRed-600">howdy</h1>
+    </>
   )
 }
 
