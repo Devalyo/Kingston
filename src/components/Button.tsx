1b57ddb31a732c4fae7a1e36c2263d4b14e8ae21
Devalyo <danieldevalho@gmail.com>
2025-03-20 16:38:58 -0300

    fix: button cursor pointer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,24 @@
 import { ArrowRight } from "lucide-react";
 
+
 interface ButtonProps {
-    arrow: boolean;
-    text: string;
-    width?: number | string;
-  }
+  arrow: boolean;
+  text: string;
+  width?: number | string;
+  
+}
 
 function Button(props : ButtonProps) {
   return (
     <div>
-        <button className={`bg-neutral-900 text-white px-8 py-3 rounded-md flex items-center gap-2 justify-center h-12 ${props.width? 'w-' + props.width : 'w-auto'}`}>
+        <button className={`cursor-pointer bg-neutral-900 text-white px-8 py-3 rounded-md flex items-center gap-2 justify-center h-12 ${props.width? 'w-' + props.width : 'w-auto'}`}>
         <span>{props.text}</span>
         {props.arrow && <ArrowRight />}
         </button>
     </div>
-  )
+)
+
 }
+  
 
 export default Button
